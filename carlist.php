2696e576e474e1ce1db5ec5865074b1e5c5efa41
Juan Valero <juanseval1@gmail.com>
2020-12-15 17:40:45 -0300

    Added modals with extra info

--- carlist.php
+++ carlist.php
@@ -3,12 +3,22 @@
     {
       "Brand": "RE Amemiya",
       "Model": "FD3S RX-7",
-      "Country": "Japan"
+      "Country": "Japan",
+      "Power": "356 BHP",
+      "Layout": "FR",
+      "Weight": "1,240 kilograms",
+      "Year": "1991",
+      "Photo": "RE Amemiya FD3S RX-7.png"
     },
     {
       "Brand": "BMW",
       "Model": "M3",
-      "Country": "Germany"
+      "Country": "Germany",
+      "Power": "415 HP",
+      "Layout": "FR",
+      "Weight": "1,655 kilograms",
+      "Year": "2007",
+      "Photo": "BMW M3.png"
     }
   ]
 }
